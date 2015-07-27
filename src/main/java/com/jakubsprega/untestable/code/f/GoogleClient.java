a92f4dc142a6e551b9e0b8e04ed41e751b05d5e7
cslysy <kubolec85@gmail.com>
2015-07-27 10:29:07 +0200

    Improve google client

--- src/main/java/com/jakubsprega/untestable/code/f/GoogleClient.java
+++ src/main/java/com/jakubsprega/untestable/code/f/GoogleClient.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 /**
  * Google client
@@ -15,7 +14,8 @@ import java.nio.charset.StandardCharsets;
  */
 public class GoogleClient {
     
-    private static final String DEFAULT_GOOGLE_API_URL = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
+    //Google api url
+    //"http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
 
     private final String googleApiUrl;
     private final Charset charset;
@@ -23,18 +23,6 @@ public class GoogleClient {
     public GoogleClient(String googleApiUrl, Charset charset) {
         this.googleApiUrl = googleApiUrl;
         this.charset = charset;
-    }
-
-    public GoogleClient() {
-        this(DEFAULT_GOOGLE_API_URL, StandardCharsets.UTF_8);
-    }
-
-    public GoogleClient(String googleApiUrl) {
-        this(googleApiUrl, StandardCharsets.UTF_8);
-    }
-    
-    public GoogleClient(Charset charset) {
-        this(DEFAULT_GOOGLE_API_URL, charset);
     }
 
     public GoogleResult search(String searchPhrase) throws Exception {
