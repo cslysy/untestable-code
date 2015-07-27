92de0909de47989c07eb0507e76f1cacf7ab6058
cslysy <kubolec85@gmail.com>
2015-07-27 10:00:49 +0200

    Improve google client

--- src/main/java/com/jakubsprega/untestable/code/f/GoogleClient.java
+++ src/main/java/com/jakubsprega/untestable/code/f/GoogleClient.java
@@ -5,28 +5,50 @@ import com.google.gson.Gson;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Google client
- * 
+ *
  * @author jakubsprega
  */
 public class GoogleClient {
+    
+    private static final String DEFAULT_GOOGLE_API_URL = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
+
+    private final String googleApiUrl;
+    private final Charset charset;
+
+    public GoogleClient(String googleApiUrl, Charset charset) {
+        this.googleApiUrl = googleApiUrl;
+        this.charset = charset;
+    }
+
+    public GoogleClient() {
+        this(DEFAULT_GOOGLE_API_URL, StandardCharsets.UTF_8);
+    }
+
+    public GoogleClient(String googleApiUrl) {
+        this(googleApiUrl, StandardCharsets.UTF_8);
+    }
+    
+    public GoogleClient(Charset charset) {
+        this(DEFAULT_GOOGLE_API_URL, charset);
+    }
 
     public GoogleResult search(String searchPhrase) throws Exception {
-        String googleApiUrl = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
-        String charset = "UTF-8";
         String googleRequestUrl = String.format(
-            "%s%s", googleApiUrl, URLEncoder.encode(searchPhrase, charset)
+            "%s%s", googleApiUrl, URLEncoder.encode(searchPhrase, charset.name())
         );
-        
+
         URL url = new URL(googleRequestUrl);
         String rawJsonGoogleResponse = CharStreams.toString(
             new InputStreamReader(url.openStream(), charset)
         );
-        
+
         GoogleResult results = new Gson().fromJson(rawJsonGoogleResponse, GoogleResult.class);
-        
+
         return results;
     }
 }
